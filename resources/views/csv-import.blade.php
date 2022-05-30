87db871e99c6a27f2e42232a3faa50db88a6642b
Jacob Tinston <jacob@surgems.co.uk>
2022-05-30 22:29:58 +0100

    Updated Layout

--- resources/views/csv-import.blade.php
+++ resources/views/csv-import.blade.php
@@ -2,24 +2,32 @@
 @section('title', 'Import Redirects')
 
 @section('content')
-    <form action="{{ cp_route('redirect-urls.import.submit') }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ cp_route('redirect-urls.import.submit') }}" method="post" enctype="multipart/form-data">
         {{ csrf_field() }}
 
-        <header>
-            <h1>Redirects Import</h1>
+        <header class="mb-3">
+            <div class="breadcrumb flex">
+                <a href="{{ cp_route('redirect-urls.dashboard') }}" class="flex-initial flex p-1 -m-1 items-center text-xs text-grey-70 hover:text-grey-90">
+                    <svg viewBox="0 0 24 24" class="align-middle h-6 w-4 rotate-180">
+                        <path fill="currentColor" fill-rule="evenodd" d="m10.414 7.05 4.95 4.95-4.95 4.95L9 15.534 12.536 12 9 8.464z"></path>
+                    </svg> 
+                    <span>Redirect Urls</span>
+                </a>
+            </div> 
+            
+            <div class="flex items-center">
+                <h1 class="flex-1">Import Redirects</h1>
+            </div>
         </header>
 
-        <article>
-            <p style="margin: 20px 0;">
-                Define URL redirects by importing a CSV/Excel file with <span>From</span>, <span>To</span> and <span>Type</span> fields. <br>
-                *NOTE: Please ensure the CSV file includes a header.
+        <div class="card content p-2 my-3 overflow-x-scroll">
+            <p class="my-2">
+                Import redirects from a CSV/XCLS file with <code>From</code>, <code>To</code> and <code>Type</code> headers.
             </p>
-        </article>
 
-        <div style="background-color:#fff;padding:30px;border-radius:5px;">
             <div>
                 <label for="file" style="font-weight:600;">CSV</label>
-                <input id="file" name="file" type="file" tabindex="1" class="input-text" accept="text/csv" style="margin:10px 0;">
+                <input id="file" name="file" type="file" tabindex="1" class="input-text mb-2 mt-1 cursor-pointer" accept="text/csv">
             </div>
 
             <div>
